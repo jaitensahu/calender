a8e37f6e9888bbf85e7ad249f2ab70b4f61f0bf9
Jaiten <Jaitensahu9@gmail.com>
2023-10-17 18:31:03 +0530

    updated

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 let new_Date=new Date();
 let arrayOfDay=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let arrayOfMonth=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-document.querySelector(".month").innerText=arrayOfMonth[new_Date.getMonth()];
-document.querySelector(".day").innerText=arrayOfDay[new_Date.getDay()];
-document.querySelector(".date").innerText=new_Date.getDate();
-document.querySelector(".year").innerText=new_Date.getFullYear();
-document.querySelector(".hour").innerText=new_Date.getHours();
-document.querySelector(".minute").innerText=new_Date.getMinutes();
-document.querySelector(".second").innerText=new_Date.getSeconds();;
-// let i=1;
-// while(i<=6){
-// settimeout(()=>{
-//     ti.innerText=new_Date.getSeconds();
-// },1000)
-// i++;
-// }
-    
-// }
+let seconds=document.querySelector(".second")
 
+function secondsUpdate(){
+    document.querySelector(".month").innerText=arrayOfMonth[new Date().getMonth()];
+    document.querySelector(".day").innerText=arrayOfDay[new Date().getDay()];
+    document.querySelector(".date").innerText=new Date().getDate();
+    document.querySelector(".year").innerText=new Date().getFullYear();
+    document.querySelector(".hour").innerText=new Date().getHours();
+    document.querySelector(".minute").innerText=new Date().getMinutes();
+}
 
-// console.log(new_Date);
+setInterval(() => {
+    seconds.innerText=new Date().getSeconds();
+    secondsUpdate();
+}, 1000);
